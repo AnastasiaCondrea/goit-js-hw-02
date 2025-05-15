bce8e4c7f44f7e311e8a5c49d2973c13f85dea9d
AnastasiaCondrea <coloratura17@gmail.com>
2025-05-15 15:28:36 +0200

    add

--- js/task-4.js
+++ js/task-4.js
@@ -22,9 +22,9 @@ return `Shipping to ${country} will cost ${priece} credits`
 }
 
 
-console.log(getShippingCost("Australia", 170)); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
-console.log(getShippingCost("China", 100)); // "Shipping to China will cost 100 credits"
-console.log(getShippingCost("Chile", 250)); // "Shipping to Chile will cost 250 credits"
-console.log(getShippingCost("Jamaica", 120)); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
 console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
